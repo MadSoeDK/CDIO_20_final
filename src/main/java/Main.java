97a9500bae2499a8561278fe006f9a5b2a22b127
Mark <mpnvip@gmail.com>
2021-11-09 16:28:14 +0100

    Created all classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import gui_main.GUI;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello");
+        GUI gui = new GUI();
+
     }
 }
