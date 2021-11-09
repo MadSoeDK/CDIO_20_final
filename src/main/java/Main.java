8a1c8f504112992fc507176312c6b48d753f45d3
Mark <mpnvip@gmail.com>
2021-11-09 17:02:56 +0100

    Changed class-names to correct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Start;
 import gui_main.GUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello");
-        GUI gui = new GUI();
 
+        GUI gui = new GUI();
     }
 }
