764858ef4de1154f4238dd581201e5700c892a15
i9ali7 <i9ali7@gmail.com>
2021-11-09 17:04:54 +0100

    Implementer Cup & Die

--- src/main/java/Cup.java
+++ src/main/java/Cup.java
@@ -1,2 +1,45 @@
+/**
+ * Represents a raflecup that contains dies.
+ * Creates 2 dies upon initialization
+ */
 public class Cup {
+
+    private final int QUANTITY = 2;
+    private int sum;
+    private int pair;
+
+    private int facevalue1;
+    private int facevalue2;
+
+    Die Die1;
+    Die Die2;
+
+    public Cup() {
+        Die1 = new Die();
+        Die2 = new Die();
+    }
+
+    public int getSum() {
+        facevalue1 = Die1.getFacevalue();
+        facevalue2 = Die2.getFacevalue();
+        sum = facevalue1 + facevalue2;
+        return sum;
+    }
+
+    public int getFacevalue1() {
+        return facevalue1;
+    }
+
+    public int getFacevalue2() {
+        return facevalue2;
+    }
+
+    public int getPair() {
+        if (facevalue1 == facevalue2) {
+            pair = 1;
+        } else {
+            pair = 0;
+        }
+        return pair;
+    }
 }
--- src/main/java/Die.java
+++ src/main/java/Die.java
@@ -1,2 +1,17 @@
+import java.util.Random;
+
 public class Die {
+
+    // Initialize Random Object
+    Random rand = new Random();
+
+    // Initialize Variables
+    private final int MAXVALUE = 6;
+    private int facevalue;
+
+    public int getFacevalue() {
+        facevalue = rand.nextInt(MAXVALUE) + 1;
+        // System.out.println(facevalue);
+        return facevalue;
+    }
 }
