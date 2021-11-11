1218994a82c6071acd66d204adfcf9f6cc044f86
i9ali7 <i9ali7@gmail.com>
2021-11-11 13:18:39 +0100

    Implementer Account

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,2 +1,30 @@
+import static java.lang.Math.abs;
+
+/**
+ * Account handles player balance
+ */
 public class Account {
+
+    private int balance;
+
+    public Account() {
+        balance = 35;
+    }
+
+    /**
+     * Changes the balance. The balance can't be negative, and is then set to 0.
+     *
+     * @param amount Positive or negative integer
+     */
+    public void setBalance(int amount) {
+        if (amount < 0 && amount > balance) {
+            balance = 0;
+        } else {
+            balance = balance + amount;
+        }
+    }
+
+    public int getBalance() {
+        return balance;
+    }
 }
