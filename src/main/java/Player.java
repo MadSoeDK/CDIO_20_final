6554d5ace86180da8d35e1cf0babf9b10b71ec69
i9ali7 <i9ali7@gmail.com>
2021-11-11 13:57:48 +0100

    Added methods

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,10 @@
 public class Player {
+    Account account = new Account();
+    public int getPlayerBalance(){
+        return account.getBalance();
+    }
+
+    public void setPlayerBalance(int amount){
+        account.setBalance(amount);
+    }
 }
