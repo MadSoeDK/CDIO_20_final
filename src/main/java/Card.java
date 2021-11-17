ed7a133896356bf53cd95aec3d7523b3c2a2b9bf
Mads <kontakt@mswebpartner.dk>
2021-11-17 11:55:38 +0100

    Made Card abstract and ready to implement subclasses

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,17 +1,6 @@
 import gui_fields.GUI_Chance;
-
 import java.awt.*;
 
-public class Card extends ChanceField {
-    protected int value;
-
-    public Card(GUI_Chance field, Card card, String name, Color color, int value) {
-        super(field, name, fieldType, color);
-        this.card = new Card[20];
-        this.value = value;
-        iniCards();
-    }
-    public void iniCards() {
-    }
-
+public abstract class Card {
+    protected GUI_Chance chance;
 }
