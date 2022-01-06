ad762bd47081052705a940ddca19b0336f48c542
Mark <mpnvip@gmail.com>
2022-01-06 11:06:39 +0100

    fixed code structure and syntax

---
+++ src/main/java/Model/Company.java
@@ -0,0 +1,40 @@
+package Model;
+
+import gui_fields.GUI_Brewery;
+import gui_fields.GUI_Shipping;
+
+import java.awt.*;
+
+public class Company extends Ownable {
+    int rent = 500;
+    String name = "Scandlines";
+    /**
+     * Constructor
+     *
+     * @param field
+     * @param //rent
+     * @param //name
+     * @param //color
+     */
+
+    public Company(GUI_Brewery field, int rent, String name, Color color) {
+        field.setTitle(name);
+        field.setBackGroundColor(color);
+        field.setSubText("$" + rent);
+        field.setDescription("My description");
+
+        this.field = field;
+        this.rent = rent;
+        this.name = name;
+        this.color = color;
+    }
+    public int getRent()
+    {
+        // Check owner of other ferries
+        //if ()
+        return rent;
+    }
+    public void setRent(int newRent) {
+        rent = newRent;
+    }
+}
