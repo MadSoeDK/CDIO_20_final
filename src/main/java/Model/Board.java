7d0652be37f2312b82f5c8f02fe0e5561a9a459d
mamm1504 <s215805@student.dtu.dk>
2022-01-06 14:08:36 +0100

    Removed unneccersary code from previus architecture. Now GUI elements are controlled in GUI Controller

--- src/main/java/Model/Board.java
+++ src/main/java/Model/Board.java
@@ -16,64 +16,60 @@ public class Board {
 
 
     private Field[] fields = {
-            new Start(new GUI_Start(),"Start", Color.WHITE, "Startfeltet"),
-            new Property(new GUI_Street(),1, "Blå", Color.CYAN),
-            new ChanceField(new GUI_Chance()),
-            new Property(new GUI_Street(),1, "Blå", Color.CYAN),
-            new Tax(new GUI_Tax(), 2000),
-            new Ferry(new GUI_Shipping()),
-            new Property(new GUI_Street(),1, "Orange", Color.ORANGE),
-            new ChanceField(new GUI_Chance()),
-            new Property(new GUI_Street(),1, "Orange", Color.ORANGE),
-            new Property(new GUI_Street(),1, "Orange", Color.ORANGE),
-            new Jail(new GUI_Jail(), 0,"På besøg", Color.WHITE),
-            new Property(new GUI_Street(),1, "Gul", Color.YELLOW),
-            new Property(new GUI_Street(),1, "SQUASH", Color.WHITE),
-            new Property(new GUI_Street(),2, "Gul", Color.YELLOW),
-            new Property(new GUI_Street(),1, "Gul", Color.YELLOW),
-            new Ferry(new GUI_Shipping()),
-            new Property(new GUI_Street(),1, "Grå", Color.GRAY),
-            new ChanceField(new GUI_Chance()),
-            new Property(new GUI_Street(),1, "Grå", Color.GRAY),
-            new Property(new GUI_Street(),1, "Grå", Color.GRAY),
-            new FreeParking(new GUI_Tax(),0,"Fri Parkering",Color.WHITE,"Modtag Penge"),
-            new Property(new GUI_Street(),1, "Rød", Color.RED),
-            new ChanceField(new GUI_Chance()),
-            new Property(new GUI_Street(),1, "Rød", Color.RED),
-            new Property(new GUI_Street(),1, "Rød", Color.RED),
-            new Ferry(new GUI_Shipping()),
-            new Property(new GUI_Street(),1, "Hvid", Color.WHITE),
-            new Property(new GUI_Street(),1, "COLA", Color.WHITE),
-            new Property(new GUI_Street(),2, "Hvid", Color.WHITE),
-            new Property(new GUI_Street(),1, "HVID", Color.WHITE),
-            new Jail(new GUI_Jail(), 3, "Gå i fængsel", Color.WHITE),
-            new Property(new GUI_Street(),1, "Gul", Color.YELLOW),
-            new Property(new GUI_Street(),1, "GUL", Color.YELLOW),
-            new ChanceField(new GUI_Chance()),
-            new Property(new GUI_Street(),1, "Gul", Color.YELLOW),
-            new Ferry(new GUI_Shipping()),
-            new ChanceField(new GUI_Chance()),
-            new Property(new GUI_Street(),1, "Lilla", Color.PINK),
-            new Property(new GUI_Street(),1, "2000 Skat", Color.WHITE),
-            new Property(new GUI_Street(),1, "Lilla", Color.PINK),
+            new Start("Start", Color.WHITE, "Startfeltet"),
+            new Property(1, "Blå", Color.CYAN),
+            new ChanceField(),
+            new Property(1, "Blå", Color.CYAN),
+            new Tax(2000),
+            new Ferry(),
+            new Property(1, "Orange", Color.ORANGE),
+            new ChanceField(),
+            new Property(1, "Orange", Color.ORANGE),
+            new Property(1, "Orange", Color.ORANGE),
+            new Jail(0,"På besøg", Color.WHITE),
+            new Property(1, "Gul", Color.YELLOW),
+            new Property(1, "SQUASH", Color.WHITE),
+            new Property(2, "Gul", Color.YELLOW),
+            new Property(1, "Gul", Color.YELLOW),
+            new Ferry(),
+            new Property(1, "Grå", Color.GRAY),
+            new ChanceField(),
+            new Property(1, "Grå", Color.GRAY),
+            new Property(1, "Grå", Color.GRAY),
+            new FreeParking(0,"Fri Parkering",Color.WHITE,"Modtag Penge"),
+            new Property(1, "Rød", Color.RED),
+            new ChanceField(),
+            new Property(1, "Rød", Color.RED),
+            new Property(1, "Rød", Color.RED),
+            new Ferry(),
+            new Property(1, "Hvid", Color.WHITE),
+            new Property(1, "COLA", Color.WHITE),
+            new Property(2, "Hvid", Color.WHITE),
+            new Property(1, "HVID", Color.WHITE),
+            new Jail(3, "Gå i fængsel", Color.WHITE),
+            new Property(1, "Gul", Color.YELLOW),
+            new Property(1, "GUL", Color.YELLOW),
+            new ChanceField(),
+            new Property(1, "Gul", Color.YELLOW),
+            new Ferry(),
+            new ChanceField(),
+            new Property(1, "Lilla", Color.PINK),
+            new Property(1, "2000 Skat", Color.WHITE),
+            new Property(1, "Lilla", Color.PINK),
     };
 
     public Board() {
-        gui = new GUI(converter(fields));
+        //gui = new GUI(converter(fields));
     }
 
-    /**
-     * Returns an array of the correct GUI_Fields type for the GUI to use
-     * @param fields
-     * @return
-     */
-    public GUI_Field[] converter(Field[] fields) {
+    /*public GUI_Field[] converter(Field[] fields) {
         GUIfields = new GUI_Field[fields.length];
         for(int i = 0; i < GUIfields.length; i++) {
             GUIfields[i] = fields[i].field;
         }
         return GUIfields;
     }
+
     public void newGame() {
         switch (gui.getUserSelection("How many players?", "2", "3", "4")) {
             case "2":
@@ -98,10 +94,6 @@ public class Board {
         }
     }
 
-    public void button(int currentPlayer) {
-        gui.getUserButtonPressed("Nu er det " + player[currentPlayer].getName() + "'s tur, rul terningen!", "Rul terning");
-    }
-
     public void guiMessage(String message){
         gui.showMessage(message);
     }
@@ -122,25 +114,29 @@ public class Board {
         return player.length;
     }
 
-    public void setDice(int sum) {
+    /*public void setDice(int sum) {
             gui.setDie(sum);
         }
 
+    public void Button(int currentPlayer) {
+        gui.getUserButtonPressed("Nu er det " + player[currentPlayer].getName() + "'s tur, rul terningen!", "Rul terning");
+    }
+
     public Player getPlayer(int number) {
         return player[number];
-        }
+    }*/
 
     public Field getField(int placement) {
             return fields[placement];
         }
 
-    public Player getCurrentPlayer(){
+    /*public Player getCurrentPlayer(){
         return player[currentPlayer];
     }
 
     public void updateCurrentPlayer(int currentPlayer){
         this.currentPlayer=currentPlayer;
-    }
+    }*/
 
     public boolean checkWinner(){
         // If no winner
@@ -152,6 +148,9 @@ public class Board {
             }
         }
         return winner;
+    }
+    public Field[] getFields() {
+        return fields;
     }
     public int getFieldsTotal() {
         return GUIfields.length;
