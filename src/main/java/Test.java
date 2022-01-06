825e5ba1ce8f2d9d51e1ef63fa5188f4a6d520ce
Bertram Kjaer <s215775@student.dtu.dk>
2022-01-06 10:26:17 +0100

    Made test case 01

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,2 +1,13 @@
-public class Test {
+import Model.Board;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldAmountTest {
+    Board board = new Board();
+
+    @Test
+    public void fieldAmount() {
+        assertTrue(board.getFieldsTotal() == 40);
+    }
+
 }
