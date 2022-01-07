973c4016b45ba00c65a067d37f50bb68f04d940e
mamm1504 <s215805@student.dtu.dk>
2022-01-07 12:38:04 +0100

    Cleaned up Property class

--- src/main/java/Model/Property.java
+++ src/main/java/Model/Property.java
@@ -2,46 +2,20 @@ package Model;
 
 import java.awt.Color;
 
-/**
- * Child of Model.Field, Used in field array, ownable game fields.
- */
 public class Property extends Ownable {
 
-    // Variables
     protected int rent;
     protected Color color;
 
-    /**
-     * Constructor
-     * @param rent
-     * @param name
-     * @param color
-     */
-
-    // Constructor
-    public Property (int rent, String name, Color color/*, String description*/) {
-        /*field.setTitle(name);
-        field.setBackGroundColor(color);
-        field.setSubText("$" + rent);
-        field.setDescription("My description");
-
-
-        this.field = field;*/
+    public Property (int rent, String name, Color color) {
         this.rent = rent;
         this.name = name;
         this.color = color;
     }
 
-    // Methods
     public int getRent() {
         return rent;
     }
-    /*public void setOwner(Player player) {
-        this.owner = player;
-    }
-    public Player getOwner() {
-        return owner;
-    }*/
     public Color getColor() {
         return color;
     }
