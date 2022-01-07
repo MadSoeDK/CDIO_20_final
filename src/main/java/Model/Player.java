5a9e52c4941be5833e2ed7d1567db240d5a4a1e9
mamm1504 <s215805@student.dtu.dk>
2022-01-07 12:32:58 +0100

    Cleaned up Player class

--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -1,41 +1,27 @@
 package Model;
 
-import Model.Account;
-import gui_fields.GUI_Car;
-import gui_fields.GUI_Player;
-import java.awt.Color;
-
 /**
  * Used in Model.Player Array, Controls Model.Account-class, used to move, earn & lose money.
  */
 public class Player {
 
-    // Variables
     private int placement;
-    private String name;
-    //private GUI_Player player;
-    private Account account;
+    private final String name;
+    int balance;
 
-    // Constructor
-    public Player(String name, int balance/*, Color color*/) {
-         account = new Account(balance);
-         //this.player = new GUI_Player(name, balance, new GUI_Car(color,color, GUI_Car.Type.CAR, GUI_Car.Pattern.FILL));
+    public Player(String name, int balance) {
+         this.balance = balance;
          this.name = name;
     }
 
-    // Methods
     public int getPlayerBalance() {
-        return account.getBalance();
+        return balance;
     }
-    public void setPlayerBalance(int amount) {account.setBalance(amount);}
-    /*public GUI_Player getPlayer(){
-        return this.player;
-    }*/
+    public void setPlayerBalance(int amount) { balance += amount; }
     public int getPlacement() {
         return placement;
     }
     public void gotoPlacement(int placement) { this.placement = placement; }
-
     public void setPlacement(int sum) {
         //placement += sum;
         placement = sum;
@@ -43,8 +29,4 @@ public class Player {
     public String getName() {
         return name;
     }
-    /*public Color getPlayerColor() {
-        return player.getPrimaryColor();
-    }*/
-
 }
