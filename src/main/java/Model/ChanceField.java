26f7dc6a7f2502a28451958b38b2acfb985a9fae
mamm1504 <s215805@student.dtu.dk>
2022-01-08 14:45:46 +0100

    added Brewery and fixed ChanceField

--- src/main/java/Model/ChanceField.java
+++ src/main/java/Model/ChanceField.java
@@ -1,15 +1,14 @@
 package Model;
 
 import java.awt.*;
-//import java.awt.Color;
 
 /**
  * Is used within the Fieldarray, is used to draw a chance card when landed on.
  */
 public class ChanceField extends Field {
 
-    public ChanceField() {
-
+    public ChanceField(String name, int placement) {
+        super(name, placement);
     }
 
 }
