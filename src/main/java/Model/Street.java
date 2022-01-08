8f170172c166175faf16ae830aee33cd35ea69c7
mamm1504 <s215805@student.dtu.dk>
2022-01-08 15:03:09 +0100

    Removed unneccersary classes

--- src/main/java/Model/Street.java
+++ src/main/java/Model/Street.java
@@ -3,10 +3,12 @@ package Model;
 public class Street extends Ownable {
 
     protected int HOUSEPRICE;
+    protected boolean mortgage;
 
     public Street (String name, int placement, String color, int[] rent, int price, int housePrice) {
         super(name, placement, color, rent, price);
         this.HOUSEPRICE = housePrice;
+        this.mortgage = false;
     }
     public int getHousePrice() {
         return HOUSEPRICE;
