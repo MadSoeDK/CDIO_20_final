6e382f37a8522fe872bf9bcfa40cecd68f027d70
Bertram Kjaer <s215775@student.dtu.dk>
2022-01-10 12:56:01 +0100

    Deleted a space

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -377,7 +377,6 @@ public class GameController {
                                     }
                                 }
                             }
-
                         }
                     }
             }
