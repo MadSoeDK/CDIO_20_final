04610f8bfc74a78e045b4093bf64ff5287986f74
Lucas Schoubye <lucasschoubye@gmail.com>
2022-01-11 20:04:32 +0100

    bugfixing

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -42,7 +42,7 @@ public class GameController {
 
         // Roll die, get value, show die
         cup.roll();
-        sum = 1; //cup.getSum();
+        sum = cup.getSum();
         gui.showDice(cup.getFacevalues()[0], cup.getFacevalues()[1]);
 
         // Move player placement - automatically updates GUI
