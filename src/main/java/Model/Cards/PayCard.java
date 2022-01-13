e604fdd67eacdab8a4daf7586c257c43eb9c1833
mads <s215805@student.dtu.dk>
2022-01-13 10:25:00 +0100

    Added ChanceCards again

---
+++ src/main/java/Model/Cards/PayCard.java
@@ -0,0 +1,23 @@
+package Model.Cards;
+
+public class PayCard extends ChanceCard {
+    private String description;
+    private String type;
+    private int value;
+
+    public PayCard(String description, String type, int value) {
+        super(description, type, value);
+        this.value = value;
+        this.description = description;
+        this.type = type;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getType() {
+        return type;
+    }
+    public int getValue() {
+        return value;
+    }
+}
