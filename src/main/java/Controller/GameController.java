507d6045271bb43d9e6d4230d2dcaef4e837a553
Mark <mpnvip@gmail.com>
2022-01-14 16:55:11 +0100

    Code clean up in GameController

--- src/main/java/Controller/GameController.java
+++ src/main/java/Controller/GameController.java
@@ -351,7 +351,6 @@ public class GameController {
                         eliminatePlayer(player, placement);
 
                         //Change ownership
-
                     } else { //Pay by mortgage
                         mortgage(player);
                     }
